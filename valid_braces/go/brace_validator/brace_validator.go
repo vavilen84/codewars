d3b3dd485d41abccc96ba4a0ede5f07e59a23a4b
Teplov <vladimir.teplov@gmail.com>
2020-08-09 12:40:24 +0300

    add "braces validator" golang solving

---
+++ valid_braces/go/brace_validator/brace_validator.go
@@ -0,0 +1,102 @@
+package brace_validator
+
+import "strings"
+
+const (
+	_ = iota
+	ROUND
+	CURLY
+	SQUARE
+)
+
+func Validate(i string) bool {
+
+	s := strings.Split(i, "")
+	opened, closed, allowed, types := getDataMaps()
+
+	if !validateInput(s, allowed) {
+		return false
+	}
+	if !validateFirstSybmol(s, opened) {
+		return false
+	}
+	if !validateLastSybmol(s, closed) {
+		return false
+	}
+	if !validateChain(s, opened, closed, types) {
+		return false
+	}
+	return true
+}
+
+func getDataMaps() (opened map[string]bool, closed map[string]bool, allowed map[string]bool, types map[string]int) {
+	types = map[string]int{
+		"(": ROUND,
+		")": ROUND,
+		"{": CURLY,
+		"}": CURLY,
+		"[": SQUARE,
+		"]": SQUARE,
+	}
+	opened = map[string]bool{
+		"(": true,
+		"[": true,
+		"{": true,
+	}
+	closed = map[string]bool{
+		")": true,
+		"]": true,
+		"}": true,
+	}
+	allowed = map[string]bool{
+		"(": true,
+		")": true,
+		"{": true,
+		"}": true,
+		"[": true,
+		"]": true,
+	}
+	return
+}
+
+func validateChain(s []string, opened map[string]bool, closed map[string]bool, types map[string]int) bool {
+	for k := range s {
+		if k == 0 {
+			continue
+		}
+		if ok := opened[s[k-1]]; ok {
+			if ok = closed[s[k]]; ok {
+				if types[s[k-1]] != types[s[k]] {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+func validateLastSybmol(i []string, closed map[string]bool) bool {
+	if ok := closed[i[len(i)-1]]; !ok {
+		return false
+	}
+	return true
+}
+
+func validateFirstSybmol(i []string, opened map[string]bool) bool {
+	if ok := opened[i[0]]; !ok {
+		return false
+	}
+	return true
+}
+
+func validateInput(i []string, allowed map[string]bool) bool {
+	if len(i) < 1 {
+		return false
+	}
+	for _, v := range i {
+		if ok := allowed[v]; !ok {
+			return false
+		}
+	}
+	return true
+}
---
+++ valid_braces/go/brace_validator/brace_validator_test.go
@@ -0,0 +1,267 @@
+package brace_validator
+
+import (
+	"testing"
+)
+
+func TestValidate(t *testing.T) {
+	type args struct {
+		i string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"Validate OK (){}[]",
+			args{
+				i: "(){}[]",
+			},
+			true,
+		},
+		{
+			"Validate OK ([{}])",
+			args{
+				i: "([{}])",
+			},
+			true,
+		},
+		{
+			"Validate FAIL (}",
+			args{
+				i: "(}",
+			},
+			false,
+		},
+		{
+			"Validate FAIL 'not allowed'",
+			args{
+				i: "not allowed",
+			},
+			false,
+		},
+		{
+			"Validate FAIL empty string",
+			args{
+				i: "",
+			},
+			false,
+		},
+		{
+			"Validate FAIL closed first",
+			args{
+				i: "}",
+			},
+			false,
+		},
+		{
+			"Validate FAIL opened last",
+			args{
+				i: "((",
+			},
+			false,
+		},
+		{
+			"Validate FAIL wrong chaining [(])",
+			args{
+				i: "[(])",
+			},
+			false,
+		},
+		{
+			"Validate FAIL wrong chaining [({})](]",
+			args{
+				i: "[({})](]",
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Validate(tt.args.i); got != tt.want {
+				t.Errorf("Validate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_validateChain(t *testing.T) {
+	opened, closed, _, types := getDataMaps()
+	type args struct {
+		s      []string
+		opened map[string]bool
+		closed map[string]bool
+		types  map[string]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"Validate OK chain with the same type, both opened",
+			args{
+				s:      []string{"(", "("},
+				opened: opened,
+				closed: closed,
+				types:  types,
+			},
+			true,
+		},
+		{
+			"Validate OK chain with the same type, last closed",
+			args{
+				s:      []string{"(", ")"},
+				opened: opened,
+				closed: closed,
+				types:  types,
+			},
+			true,
+		},
+		{
+			"Validate OK chain with opened different types",
+			args{
+				s:      []string{"(", "["},
+				opened: opened,
+				closed: closed,
+				types:  types,
+			},
+			true,
+		},
+		{
+			"Validate FAIL chain",
+			args{
+				s:      []string{"(", "}"},
+				opened: opened,
+				closed: closed,
+				types:  types,
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateChain(tt.args.s, tt.args.opened, tt.args.closed, tt.args.types); got != tt.want {
+				t.Errorf("validateChain() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_validateFirstSybmol(t *testing.T) {
+	opened, _, _, _ := getDataMaps()
+	type args struct {
+		i      []string
+		opened map[string]bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"Validate FAIL first closed symbol",
+			args{
+				i:      []string{")"},
+				opened: opened,
+			},
+			false,
+		},
+		{
+			"Validate OK",
+			args{
+				i:      []string{"("},
+				opened: opened,
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateFirstSybmol(tt.args.i, tt.args.opened); got != tt.want {
+				t.Errorf("validateFirstSybmol() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_validateInput(t *testing.T) {
+	_, _, allowed, _ := getDataMaps()
+
+	var allowedSlice []string
+	for k := range allowed {
+		allowedSlice = append(allowedSlice, k)
+	}
+
+	type args struct {
+		i       []string
+		allowed map[string]bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"Validate FAIL not allowed symbols",
+			args{
+				i:       []string{"not allowed", "%", "+", "", " "},
+				allowed: allowed,
+			},
+			false,
+		},
+		{
+			"Validate OK",
+			args{
+				i:       allowedSlice,
+				allowed: allowed,
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateInput(tt.args.i, tt.args.allowed); got != tt.want {
+				t.Errorf("validateInput() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_validateLastSybmol(t *testing.T) {
+	_, closed, _, _ := getDataMaps()
+	type args struct {
+		i      []string
+		closed map[string]bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"Validate FAIL last opened symbol",
+			args{
+				i:      []string{"("},
+				closed: closed,
+			},
+			false,
+		},
+		{
+			"Validate OK",
+			args{
+				i:      []string{")"},
+				closed: closed,
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateLastSybmol(tt.args.i, tt.args.closed); got != tt.want {
+				t.Errorf("validateLastSybmol() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
